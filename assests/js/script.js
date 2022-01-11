3850aeb880609921007c740c3b962bccd46eac8c
immemx <eayresm4@gmail.com>
2022-01-11 14:35:45 -0500

    Drop down menu function

--- assests/js/script.js
+++ assests/js/script.js
@@ -5,9 +5,23 @@ var createTaskHandler = function(event) {
 
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // Create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "this is a new task.";
+
+    // Create div to hold task info and ass to list item
+    var taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.className = "task-info"
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='taskname'>" + taskNameInput + "</h3><span class='task-type'>" +taskTypeInput + "</span>"
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
     taskToDoEl.appendChild(listItemEl);
 };
 
