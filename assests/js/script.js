b7b8b98d15a5979e932a78464f67119577700e65
immemx <eayresm4@gmail.com>
2022-01-18 12:43:22 -0500

    add data persistence

--- assests/js/script.js
+++ assests/js/script.js
@@ -227,7 +227,51 @@ var saveTasks = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
+var loadTask = function() {
+    tasks = localStorage.getItem("tasks");
+
+    if (tasks === null) {
+        tasks = []
+        return false;
+    }
+
+    tasks = JSON.parse(tasks);
+
+    for (var i = 0; i < tasks.length; i++){
+        tasks[i].id = taskIdCounter
+        console.log(tasks[i])
+        var listItemEl = document.createElement("li")
+        listItemEl.className = "task-item";
+        listItemEl.setAttribute('data-task-id', tasks[i].id)
+        var taskInfoEl = document.createElement('div')
+        taskInfoEl.className = "task-info";
+        taskInfoEl.innerHTML = "<h3 class='task-name'>" + tasks[i].name + "</h3><span class='task-type'>" + tasks[i].type + "</span>";
+        listItemEl.appendChild(taskInfoEl);
+        var taskActionsEl = createTaskActions(tasks[i].id)
+        listItemEl.appendChild(taskActionsEl);
+        console.log(listItemEl);
+
+        if (tasks[i].status === "to do") {
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 0
+            taskToDoEl.appendChild(listItemEl);
+        } 
+        else if (tasks[i].status === "in progress") {
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 1
+            tasksInProgressEl.appendChild(listItemEl)
+        }
+        else if (tasks[i].status === "complete") {
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 2
+            tasksCompletedEl.appendChild(listItemEl);
+        }
+
+        taskIdCounter++
+        console.log(listItemEl)
+    }
+}
+
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
 pageContentEl.addEventListener("click", taskButtonHandler);
 
 formEl.addEventListener("submit", taskFormHandler);
+
+loadTask();
